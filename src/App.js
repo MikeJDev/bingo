b1d9067ca18430c12a762ebfb2918deb047de0bc
Mike Janes <ukmjanes@gmail.com>
2020-01-22 11:30:30 -0500

    building initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Board from './components/Board'
+import TextInput from './components/TextInput'
+import { findByLabelText } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <div className='board'>
+        <Board></Board>
+      </div>
+      <div className='textInput'>
+        <TextInput></TextInput>
+      </div>
     </div>
   );
 }
