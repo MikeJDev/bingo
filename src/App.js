ed14436af20b965b4cb37c2644466b4f1d502ff8
Mike Janes <ukmjanes@gmail.com>
2020-01-22 12:10:29 -0500

    building structur for text input

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Board from './components/Board'
 import TextInput from './components/TextInput'
-import { findByLabelText } from '@testing-library/react';
+
 
 function App() {
   return (
--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -5,7 +5,34 @@ export default class TextInput extends React.Component {
   render() {
     return (
       <div>
-        text input
+        <textarea placeholder='input values seperated by commas'></textarea>
+        {/* <form>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          <input type='text' name='input1'></input>
+          
+        </form> */}
+
+
       </div>
     )
   }
