cea0c1733e680678ee75fc3c7eb22a1ff0075b7c
Mike Janes <ukmjanes@gmail.com>
2020-03-04 10:34:40 -0500

    added default state for testing

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -5,8 +5,11 @@ import {
 } from '../action-types/index'
 
 const initialState = {
-  items: [],
-  displayArray: []
+  items: ['American flag', 'city skyline', 'barn', 'national park sign', 'cow', 'state line', 'mountain', 'tree', 'RR crossing', 'bridge', 
+    'camper', 'wild flowers', 'time zone line', 'lake', 'motorcycle', 'rest area', 'police car', 'historic highway', 'windmill', 'boat',
+    'horse', 'bus', 'bird', 'traffic cone'
+],
+  // displayArray: ['hi']
 }
 
 function rootReducer(state = initialState, action) {
