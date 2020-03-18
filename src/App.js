c49b6332888eb3c547915d26ab91b86102eaa8f0
MikeJDev <ukmjanes@gmail.com>
2020-03-18 14:04:32 -0400

    added a board name - not currently hooked up to redux

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Board from './components/Board'
 import TextInput from './components/TextInput'
 import ButtonsContainer from './components/ButtonsContainer/index'
+import BoardName from './components/BoardName/index'
 
 function App() {
   return (
     <div className='AppContainer'>
       <div className="App">
+        <BoardName></BoardName>
         <div className='board'>
           <Board></Board>
           <ButtonsContainer></ButtonsContainer>
---
+++ src/components/BoardName/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../BoardName/index.css'
+
+function BoardName() {
+  return (
+    <div className='boardname'>
+      Travel Bingo
+    </div>
+  )
+}
+
+export default BoardName
